f2462b58727847f6b145ffdeebc5ee6c9c3e9465
aadhyagupta <aadhyagupta@yahoo.com>
2016-04-23 17:04:15 -0500

    adding base flask

---
+++ app/views.py
@@ -0,0 +1,13 @@
+sys.path.insert(2, '/usr/local/lib/python2.7/site-packages')
+from flask import Flask, render_template, request, jsonify, send_from_directory, redirect, url_for
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+@app.route('/')
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    return render_template('base.html', data=render_data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
