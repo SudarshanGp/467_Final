a7595739cb426814c8625c6fc02aa4edde71c9f9
Sudarshan Govindaprasad <Sudarshan@eduroamprvnat-172-21-96-185.near.illinois.edu>
2016-04-24 14:05:27 -0500

    updating line parseR

--- app/line_parser.py
+++ app/line_parser.py
@@ -8,3 +8,35 @@ for col in data_food.columns:
     if 'Unnamed' in col:
         del data_food[col]
 data_susan = pd.read_csv('static/res/curr_dataset_food_susan.csv')
+for col in data_susan.columns:
+    if 'Unnamed' in col:
+        del data_susan[col]
+
+susan_dict = {}
+for idx, row in data_susan.iterrows():
+	print(idx)
+	susan_dict[row['Date']] = {}
+	print(row)
+	breakfast = row['Breakfast']
+	# Parse breakfast
+	breakfast_list = []
+	if isinstance(breakfast, basestring) is True:
+		breakfast_list = breakfast.split(',')
+	lunch = row['Lunch']
+	lunch_list = []
+	if isinstance(lunch, basestring) is True:
+		lunch_list = lunch.split(',')
+	dinner = row['Dinner']
+	dinner_list = []
+	if isinstance(dinner, basestring) is True:
+		print(dinner)
+		dinner_list = dinner.split(',')
+	snacks = row['Snacks']
+	snacks_list = []
+	if isinstance(snacks, basestring) is True:
+		snacks_list = snacks.split(',')
+	drinks = row['Drinks']
+	drinks_list = []
+	if isinstance(drinks, basestring) is True:
+		drinks_list = drinks.split(',')
+	print(breakfast_list, lunch_list, dinner_list, snacks_list, drinks_list)
