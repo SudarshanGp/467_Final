9cf6e0993bface3c7adbc2edd9b2c8a9834ad100
Kanishk Thareja <kanishkt@gmail.com>
2016-04-24 17:01:44 -0500

    Fixed formatting

--- app/line_parser.py
+++ app/line_parser.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.insert(2, '/usr/local/lib/python2.7/site-packages')
 import pandas as pd
 import numpy as np
@@ -6,138 +7,139 @@ import pprint as pprint
 import itertools
 import json
 
+
 def parse_data(data):
-	data_dict = {}
-	for idx, row in data.iterrows():
-		data_dict[row['Date']] = {}
-		# print(row)
-		breakfast = row['Breakfast']
-		breakfast_list = []
-		if isinstance(breakfast, basestring) is True:
-			breakfast_list = breakfast.split(',')
-			if len(breakfast_list) > 0:
-				for key, value in enumerate(breakfast_list):
-					if 'Breakfast' in data_dict[row['Date']].keys():
-						temp_dict = data_food.loc[data_food['Food'] == value.strip()].T.to_dict()
-						temp_dict = temp_dict[temp_dict.keys()[0]]
-						data_dict[row['Date']]['Breakfast'].append(temp_dict)
-					else:
-						temp_dict = data_food.loc[data_food['Food'] == value.strip()].T.to_dict()
-						temp_dict = temp_dict[temp_dict.keys()[0]]
-						data_dict[row['Date']]['Breakfast'] = [temp_dict]
-		else:
-			print("NO Breakfast")
-			data_dict[row['Date']]['Breakfast'] = []
+    data_dict = {}
+    for idx, row in data.iterrows():
+        data_dict[row['Date']] = {}
+        # print(row)
+        breakfast = row['Breakfast']
+        breakfast_list = []
+        if isinstance(breakfast, basestring) is True:
+            breakfast_list = breakfast.split(',')
+            if len(breakfast_list) > 0:
+                for key, value in enumerate(breakfast_list):
+                    if 'Breakfast' in data_dict[row['Date']].keys():
+                        temp_dict = data_food.loc[data_food['Food'] == value.strip()].T.to_dict()
+                        temp_dict = temp_dict[temp_dict.keys()[0]]
+                        data_dict[row['Date']]['Breakfast'].append(temp_dict)
+                    else:
+                        temp_dict = data_food.loc[data_food['Food'] == value.strip()].T.to_dict()
+                        temp_dict = temp_dict[temp_dict.keys()[0]]
+                        data_dict[row['Date']]['Breakfast'] = [temp_dict]
+        else:
+            print("NO Breakfast")
+            data_dict[row['Date']]['Breakfast'] = []
 
-		lunch = row['Lunch']
-		lunch_list = []
-		if isinstance(lunch, basestring) is True:
-			lunch_list = lunch.split(',')
-			if len(lunch_list) > 0:
-				for key, value in enumerate(lunch_list):
-						if 'Lunch' in data_dict[row['Date']].keys():
-							temp_dict = data_food.loc[data_food['Food'] == value.strip()].T.to_dict()
-							temp_dict = temp_dict[temp_dict.keys()[0]]
-							data_dict[row['Date']]['Lunch'].append(temp_dict)
-						else:
-							temp_dict = data_food.loc[data_food['Food'] == value.strip()].T.to_dict()
-							temp_dict = temp_dict[temp_dict.keys()[0]]
+        lunch = row['Lunch']
+        lunch_list = []
+        if isinstance(lunch, basestring) is True:
+            lunch_list = lunch.split(',')
+            if len(lunch_list) > 0:
+                for key, value in enumerate(lunch_list):
+                    if 'Lunch' in data_dict[row['Date']].keys():
+                        temp_dict = data_food.loc[data_food['Food'] == value.strip()].T.to_dict()
+                        temp_dict = temp_dict[temp_dict.keys()[0]]
+                        data_dict[row['Date']]['Lunch'].append(temp_dict)
+                    else:
+                        temp_dict = data_food.loc[data_food['Food'] == value.strip()].T.to_dict()
+                        temp_dict = temp_dict[temp_dict.keys()[0]]
 
-							data_dict[row['Date']]['Lunch'] = [temp_dict]
-		else:
-			print("NO Lunch")
-			data_dict[row['Date']]['Lunch'] = []
+                        data_dict[row['Date']]['Lunch'] = [temp_dict]
+        else:
+            print("NO Lunch")
+            data_dict[row['Date']]['Lunch'] = []
 
-		dinner = row['Dinner']
-		dinner_list = []
-		if isinstance(dinner, basestring) is True:
-			dinner_list = dinner.split(',')
-			if len(dinner_list) > 0:
-				for key, value in enumerate(dinner_list):
-						if 'Dinner' in data_dict[row['Date']].keys():
-							temp_dict = data_food.loc[data_food['Food'] == value.strip()].T.to_dict()
-							temp_dict = temp_dict[temp_dict.keys()[0]]
-							data_dict[row['Date']]['Dinner'].append(temp_dict)
-						else:
-							temp_dict = data_food.loc[data_food['Food'] == value.strip()].T.to_dict()
-							temp_dict = temp_dict[temp_dict.keys()[0]]
-							data_dict[row['Date']]['Dinner'] = [temp_dict]
-		else:
-			print("NO DINNEr")
-			data_dict[row['Date']]['Dinner'] = []
+        dinner = row['Dinner']
+        dinner_list = []
+        if isinstance(dinner, basestring) is True:
+            dinner_list = dinner.split(',')
+            if len(dinner_list) > 0:
+                for key, value in enumerate(dinner_list):
+                    if 'Dinner' in data_dict[row['Date']].keys():
+                        temp_dict = data_food.loc[data_food['Food'] == value.strip()].T.to_dict()
+                        temp_dict = temp_dict[temp_dict.keys()[0]]
+                        data_dict[row['Date']]['Dinner'].append(temp_dict)
+                    else:
+                        temp_dict = data_food.loc[data_food['Food'] == value.strip()].T.to_dict()
+                        temp_dict = temp_dict[temp_dict.keys()[0]]
+                        data_dict[row['Date']]['Dinner'] = [temp_dict]
+        else:
+            print("NO DINNEr")
+            data_dict[row['Date']]['Dinner'] = []
 
-		snacks = row['Snacks']
-		snacks_list = []
-		if isinstance(snacks, basestring) is True:
-			snacks_list = snacks.split(',')
-			if len(snacks_list) > 0:
-				for key, value in enumerate(snacks_list):
-						if 'Snacks' in data_dict[row['Date']].keys():
-							temp_dict = data_food.loc[data_food['Food'] == value.strip()].T.to_dict()
-							temp_dict = temp_dict[temp_dict.keys()[0]]
-							data_dict[row['Date']]['Snacks'].append(temp_dict)
-						else:
-							temp_dict = data_food.loc[data_food['Food'] == value.strip()].T.to_dict()
-							temp_dict = temp_dict[temp_dict.keys()[0]]
-							data_dict[row['Date']]['Snacks'] = [temp_dict]
-		else:
-			print("NO Snacks")
-			data_dict[row['Date']]['Snacks'] = []
-		drinks = row['Drinks']
-		drinks_list = []
-		if isinstance(drinks, basestring) is True:
-			drinks_list = drinks.split(',')
-			if len(drinks_list) > 0:
-				for key, value in enumerate(drinks_list):
-						if 'Drinks' in data_dict[row['Date']].keys():
-							temp_dict = data_food.loc[data_food['Food'] == value.strip()].T.to_dict()
-							temp_dict = temp_dict[temp_dict.keys()[0]]
-							data_dict[row['Date']]['Drinks'].append(temp_dict)
-						else:
-							temp_dict = data_food.loc[data_food['Food'] == value.strip()].T.to_dict()
-							temp_dict = temp_dict[temp_dict.keys()[0]]
-							data_dict[row['Date']]['Drinks'] = [temp_dict]
-		else:
-			data_dict[row['Date']]['Drinks'] = []
-	return data_dict
+        snacks = row['Snacks']
+        snacks_list = []
+        if isinstance(snacks, basestring) is True:
+            snacks_list = snacks.split(',')
+            if len(snacks_list) > 0:
+                for key, value in enumerate(snacks_list):
+                    if 'Snacks' in data_dict[row['Date']].keys():
+                        temp_dict = data_food.loc[data_food['Food'] == value.strip()].T.to_dict()
+                        temp_dict = temp_dict[temp_dict.keys()[0]]
+                        data_dict[row['Date']]['Snacks'].append(temp_dict)
+                    else:
+                        temp_dict = data_food.loc[data_food['Food'] == value.strip()].T.to_dict()
+                        temp_dict = temp_dict[temp_dict.keys()[0]]
+                        data_dict[row['Date']]['Snacks'] = [temp_dict]
+        else:
+            print("NO Snacks")
+            data_dict[row['Date']]['Snacks'] = []
+        drinks = row['Drinks']
+        drinks_list = []
+        if isinstance(drinks, basestring) is True:
+            drinks_list = drinks.split(',')
+            if len(drinks_list) > 0:
+                for key, value in enumerate(drinks_list):
+                    if 'Drinks' in data_dict[row['Date']].keys():
+                        temp_dict = data_food.loc[data_food['Food'] == value.strip()].T.to_dict()
+                        temp_dict = temp_dict[temp_dict.keys()[0]]
+                        data_dict[row['Date']]['Drinks'].append(temp_dict)
+                    else:
+                        temp_dict = data_food.loc[data_food['Food'] == value.strip()].T.to_dict()
+                        temp_dict = temp_dict[temp_dict.keys()[0]]
+                        data_dict[row['Date']]['Drinks'] = [temp_dict]
+        else:
+            data_dict[row['Date']]['Drinks'] = []
+    return data_dict
+
 
 def add_to_list(data, name):
-	global id_counter
-	global json_data
-	for key, value in data.iteritems():
-		# Each value is a day
-		# temp_dict = {'Person':name, value}
-		breakfast = value['Breakfast']
-		for key, val in enumerate(breakfast):
-			val['Person'] = name
-			val['id'] = id_counter
-			json_data.append(val)
-			id_counter = id_counter + 1
-		lunch = value['Lunch']
-		for key, val in enumerate(lunch):
-			val['Person'] = name
-			val['id'] = id_counter
-			json_data.append(val)
-			id_counter = id_counter + 1
-		dinner = value['Dinner']
-		for key, val in enumerate(dinner):
-			val['Person'] = name
-			val['id'] = id_counter
-			json_data.append(val)
-			id_counter = id_counter + 1
-		snacks = value['Snacks']
-		for key, val in enumerate(snacks):
-			val['Person'] = name
-			val['id'] = id_counter
-			json_data.append(val)
-			id_counter = id_counter + 1
-		drinks = value['Drinks']
-		for key, val in enumerate(drinks):
-			val['Person'] = name
-			val['id'] = id_counter
-			json_data.append(val)
-			id_counter = id_counter + 1
-		
+    global id_counter
+    global json_data
+    for key, value in data.iteritems():
+        # Each value is a day
+        # temp_dict = {'Person':name, value}
+        breakfast = value['Breakfast']
+        for key, val in enumerate(breakfast):
+            val['Person'] = name
+            val['id'] = id_counter
+            json_data.append(val)
+            id_counter = id_counter + 1
+        lunch = value['Lunch']
+        for key, val in enumerate(lunch):
+            val['Person'] = name
+            val['id'] = id_counter
+            json_data.append(val)
+            id_counter = id_counter + 1
+        dinner = value['Dinner']
+        for key, val in enumerate(dinner):
+            val['Person'] = name
+            val['id'] = id_counter
+            json_data.append(val)
+            id_counter = id_counter + 1
+        snacks = value['Snacks']
+        for key, val in enumerate(snacks):
+            val['Person'] = name
+            val['id'] = id_counter
+            json_data.append(val)
+            id_counter = id_counter + 1
+        drinks = value['Drinks']
+        for key, val in enumerate(drinks):
+            val['Person'] = name
+            val['id'] = id_counter
+            json_data.append(val)
+            id_counter = id_counter + 1
 
 
 data_food = pd.read_csv('static/res/curr_dataset_food_up.csv')
@@ -183,6 +185,4 @@ add_to_list(data_kanishk, "Kanishk Thajera")
 add_to_list(data_aadhya, "Aadhya Gupta")
 
 with open('line_data.json', 'w') as outfile:
-    json.dump(json_data, outfile)
-
-
+    json.dump(json_data, outfile, indent=4)
