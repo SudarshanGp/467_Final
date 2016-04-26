7a7b0b785bbbcb44cc64674a2f2a93cfd5b68f98
aadhyagupta <aadhyagupta@yahoo.com>
2016-04-26 18:11:11 -0500

    pushing bar chart

--- app/static/js/barChart.js
+++ app/static/js/barChart.js
@@ -1,8 +1,7 @@
 function barcreate(file_path){
-    console.log("file patgh");
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    width = 400 - margin.left - margin.right,
+    height = 350 - margin.top - margin.bottom;
 
 var x = d3.scale.ordinal()
     .rangeRoundBands([0, width], .1);
@@ -16,22 +15,19 @@ var xAxis = d3.svg.axis()
 
 var yAxis = d3.svg.axis()
     .scale(y)
-    .orient("left")
-    .ticks(10, "%");
-
+    .orient("left");
+jQuery('#barchart').html('');
 var svg = d3.select("#barchart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.json("../static/res/radar_data_"+file_path+"_cost.json", function(error, data) {
+d3.tsv("../static/res/radar_data_"+file_path+"_cost.tsv", type,function(error, data) {
     //console.log("in here");
-  if (error) throw error;
-
-    console.log(data);
-  x.domain(data.map(function(d) { return d.keys(); }));
-  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+  if (error) console.log(error);
+  x.domain(data.map(function(d) { return d.name; }));
+  y.domain([0, d3.max(data, function(d) { console.log(d.cost);return d.cost; })]);
 
   svg.append("g")
       .attr("class", "x axis")
@@ -43,23 +39,23 @@ d3.json("../static/res/radar_data_"+file_path+"_cost.json", function(error, data
       .call(yAxis)
     .append("text")
       .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
+      .attr("y", -18)
+      .attr("dy", "-11 em")
       .style("text-anchor", "end")
-      .text("Frequency");
+      .text("Cost in Dollars");
 
   svg.selectAll(".bar")
       .data(data)
     .enter().append("rect")
       .attr("class", "bar")
-      .attr("x", function(d) { return x(d.letter); })
+      .attr("x", function(d) { return x(d.name); })
       .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.frequency); })
-      .attr("height", function(d) { return height - y(d.frequency); });
+      .attr("y", function(d) { return y(d.cost); })
+      .attr("height", function(d) { return height - y(d.cost); });
 });
-
-function type(d) {
-  d.frequency = +d.frequency;
+  function type(d) {
+  d.cost = +d.cost;
   return d;
 }
+
 }
